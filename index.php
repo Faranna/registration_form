6d45c62c5049612ff503873e25cb826174252c8e
farhana <farhana17mar@gmail.com>
2024-08-02 13:46:30 +0600

    reg done

--- index.php
+++ index.php
@@ -18,21 +18,26 @@ include("database.php");
 <body>
     <h1 class="header">Registration Form</h1>
     <div class="container">
-        <div class="mb-3">
-            <label class="form-label">Email address</label>
-            <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
-            <input type="text" class="form-control" name="name" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label for="inputPassword5" class="form-label">Password</label>
-            <input type="password" id="inputPassword5" name="password" class="form-control" aria-describedby="passwordHelpBlock">
-            <div id="passwordHelpBlock" class="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+        <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com">
             </div>
-        </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                <input type="text" class="form-control" name="name" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="inputPassword5" class="form-label">Password</label>
+                <input type="password" id="inputPassword5" name="password" class="form-control" aria-describedby="passwordHelpBlock">
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+            <input type="submit" Submit>
+
+
+        </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
@@ -42,5 +47,24 @@ include("database.php");
 
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    if (empty($name)) {
+        echo "your username is empty";
+    } else if (empty($email)) {
+        echo "your email is empty plz input u dipshit";
+    } elseif (empty($password)) {
+        echo "your password field is empty";
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users (email, name, pass) VALUES ( '$email', '$name', '$hash')";
+        mysqli_query($conn, $sql);
+        echo "you are now registered";
+    }
+}
+mysqli_close($conn);
 ?>
